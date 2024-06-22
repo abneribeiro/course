370791697039d57a8b1d5dc6b483370d11bcbea7
seu nome <eliasabner38@gmail.com>
2024-06-22 22:50:37 +0100

    Add: router for API endpoints

---
+++ src/router.ts
@@ -0,0 +1,26 @@
+import {Router} from 'express'
+
+const router = Router()
+
+router.get('/product', ()=>{})
+router.get('/product/:id', ()=>{})
+router.put('/product/:id', ()=>{})
+router.post('/product', ()=>{})
+router.put('/product/:id', ()=>{})
+
+
+router.get('/update', ()=>{})
+router.get('/update/:id', ()=>{})
+router.put('/update/:id', ()=>{})
+router.post('/update', ()=>{})
+router.put('/update/:id', ()=>{})
+
+
+
+router.get('/updatepoint', ()=>{})
+router.get('/updatepoint/:id', ()=>{})
+router.put('/updatepoint/:id', ()=>{})
+router.post('/updatepoint', ()=>{})
+router.put('/updatepoint/:id', ()=>{})
+
+export default router
