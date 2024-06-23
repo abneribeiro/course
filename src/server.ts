ea24af157051c0baf85f9a24347ff54a39ce2f09
seu nome <eliasabner38@gmail.com>
2024-06-23 14:21:41 +0100

    chore: Update server.ts to include CORS, logging, and body parsing

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
 import express from 'express'
 import router from './router'
-
+import morgan from 'morgan'
+import cors from 'cors'
+import { protect } from './modules/auth'
 
 const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World' })   
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app
